d0c44ee10db3832e8b9486303cccee4e0854a6d6
humin <694904679@qq.com>
2023-04-16 22:41:56 +0800

    爬虫一键自动化执行，代理自动循环爬取

--- main.py
+++ main.py
@@ -1,9 +1,53 @@
 from scrapy.cmdline import execute
+from multiprocessing import  Process
 import os
+import time
 import sys
+
+class FreeProxyProcess(Process): 
+    def __init__(self,name):
+        super(FreeProxyProcess,self).__init__()
+        self.name = name
+
+    def run(self):
+        os.system('scrapy crawl free_proxy_05')
+
+class RunRedis(Process): 
+    def __init__(self,name):
+        super(RunRedis,self).__init__()
+        self.name = name
+
+    def run(self):
+        os.system("E:\\pythonCode\\DATA_BASE\Redis-x64-5.0.14.1\\redis-server.exe")
+
+class LianjiaProcess(Process): 
+    def __init__(self,name):
+        super(LianjiaProcess,self).__init__()
+        self.name = name
+
+    def run(self):
+        os.system("scrapy crawl Lianjia_home")
+
 if __name__ == "__main__":
-    # sys.path.append(os.path.dirname(os.path.abspath(__file__)))
     # execute(['scrapy', 'crawl', 'free_proxy_05'])
-    execute(['scrapy', 'crawl', 'Lianjia_home'])
+    # execute(['scrapy', 'crawl', 'Lianjia_home'])
+    freeProxy = FreeProxyProcess("freeProxy") 
+    runRedis = RunRedis('runRedis')
+    lianjiaHome = LianjiaProcess('lianjiaHome')
+    processList = [runRedis, freeProxy, lianjiaHome]
+    for i in processList:
+        i.start()
+        time.sleep(5)
+    while lianjiaHome.is_alive():
+        if freeProxy.is_alive():
+            time.sleep(10*60)
+            freeProxy.run()
+    runRedis.terminate()
+    freeProxy.terminate()
+
+    for i in processList:
+        i.join()
+
+
 
 
