043eaea711fc66a16e8916c312332ecf17873a9b
humin <694904679@qq.com>
2023-04-29 21:22:15 +0800

    增加新房爬虫

--- lianjia_home/spiders/__init__.py
+++ lianjia_home/spiders/__init__.py
@@ -2,3 +2,6 @@
 #
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
+
+# scrapy crawl free_proxy_05
+# scrapy genspider -t basic spiderNAme "baidu.com"
